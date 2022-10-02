47c0d9f018e566405c18a05d027865f02088e637
Evelyn Sionov <evesionov@gmail.com>
2022-10-02 15:32:31 -0400

    10/1/22 home push

--- src/Point.java
+++ src/Point.java
@@ -3,26 +3,55 @@ public class Point
     private int x;
     private int y;
 
-    public Box(int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public Box(int x) {
+    public Point(int x) {
         this.x = x;
         this.y = x;
     }
 
-    public Box() {
+    public Point() {
         this.x = 0;
         this.y = 0;
     }
 
-    public int getX{
-        return x;
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public void setX (int newX) { x = newX; }
+
+    public void setY (int newY) { y = newY; }
+
+    public String coordinate() {
+        return "(" + x + " , " + y + ")";
     }
 
-    public int getY{
-        return y;
-    }
+    public String quadrant() {
+        String quadrant;
+        if (x > 0 && y > 0) {
+            quadrant = "I";
+        } else {
+            if (x < 0 && y > 0) {
+                quadrant = "II";
+            } else {
+                if (x < 0 && y < 0) {
+                    quadrant = "III";
+                } else {
+                    if (x > 0 && y < 0) {
+                        quadrant = "IV";
+                    } else {
+                        if (x == 0 && y == 0) {
+                            quadrant = "on the origin";
+                        } else {
+                                quadrant = "on an axis";
+                            }
+                        }
+                    }
+                }
+            }
+        return quadrant;
+        }
 }
